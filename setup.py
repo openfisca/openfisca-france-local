7d96d7d6460c289f12b41d43e2212e00de2e4193
m.delsaut <mdelsaut@ubuntu.(none)>
2016-08-05 16:17:46 +0200

    Renommage fichiers et classe

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Openfisca-MA_COLLECTIVITE",
+    name="Openfisca-RennesMetropole",
     version="1.0.0",
     description="Plugin OpenFisca pour les aides sociales de ma collectivité",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    author="Ma collectivité, Incubateur de Services Numériques (SGMAP)",
+    author="Rennes Métropole, Incubateur de Services Numériques (SGMAP)",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
