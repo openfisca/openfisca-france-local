4da02c368b010804215d2b8d7eb2fd649a160319
Thomas Guillet <guillet.thomas@gmail.com>
2018-01-19 12:33:36 +0100

    Allow new OpenFisca versions

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-BrestMetropole",
-    version="0.0.1",
+    version="0.0.2",
     description="Extension OpenFisca pour Brest métropole",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
     packages=find_packages(),
     include_package_data=True,
     install_requires = [
-        'OpenFisca-Core >= 20, < 21',
-        'OpenFisca-France >= 18.11, < 20'
+        'OpenFisca-Core >= 20, < 22',
+        'OpenFisca-France >= 18.11, < 20.1'
         ],
     extras_require = {
         'test': [
