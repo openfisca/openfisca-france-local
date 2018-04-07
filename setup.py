cd9a16bf3aab00b27787f2de8ce189a0bea8dbc9
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-07 12:06:03 +0200

    Update version contraints

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-BrestMetropole",
-    version="0.0.2",
+    version="0.0.3",
     description="Extension OpenFisca pour Brest métropole",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
     packages=find_packages(),
     include_package_data=True,
     install_requires = [
-        'OpenFisca-Core >= 20, < 22',
-        'OpenFisca-France >= 18.11, < 20.1'
+        'OpenFisca-Core >= 22, < 23',
+        'OpenFisca-France >= 20.7, < 22'
         ],
     extras_require = {
         'test': [
