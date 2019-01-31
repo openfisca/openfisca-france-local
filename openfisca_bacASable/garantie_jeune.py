e7c685d833b0b464c1e8430fd01f70c855301ffa
Thomas Guillet <guillet.thomas@gmail.com>
2019-01-31 13:06:41 +0100

    Ajoute le calcul de l'allocation maximale de la GJ

--- openfisca_bacASable/garantie_jeune.py
+++ openfisca_bacASable/garantie_jeune.py
@@ -20,3 +20,21 @@ class garantie_jeune_neet(Variable):
         not_in_training = no_indemnites_stage * no_revenus_stage_formation_pro
 
         return not_in_employment * not_in_education * not_in_training
+
+
+class garantie_jeune_max(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Montant maximal de l'allocation Garantie Jeune"
+    reference = [
+        'Article D5131-20 du code du travail',
+        'https://www.legifrance.gouv.fr/affichCodeArticle.do;jsessionid=DED54A598193DDE1DF59E0AE16BDE87D.tplgfr21s_3?idArticle=LEGIARTI000033709227&cidTexte=LEGITEXT000006072050'
+        ]
+
+    def formula(individu, period, parameters):
+        params = parameters(period).prestations.minima_sociaux.rsa
+        montant_base = params.montant_de_base_du_rsa
+        taux_1_personne = params.forfait_logement.taux_1_personne
+
+        return (individu('age', period) > 0) * montant_base * (1 - taux_1_personne)
