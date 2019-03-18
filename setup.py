a74e42e8055f2a77f8ecf990d713d9b2448578e4
Thomas Guillet <guillet.thomas@gmail.com>
2019-03-18 17:56:06 +0100

    Rend compatible à Core v27 et France v37

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 25, < 27',
-        'OpenFisca-France >= 32.2, < 36'
+        'OpenFisca-Core >= 25, < 28',
+        'OpenFisca-France >= 32.2, < 38'
     ],
     extras_require = {
         'test': 'nose'
