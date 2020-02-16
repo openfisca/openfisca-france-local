c650215e36f03ed247eeebfc85c7d6afceb3e998
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-16 12:57:32 +0100

    Ajoute les règles pour les demandeur d'emploi indemnisés

---
+++ openfisca_france_local/metropoles/toulouse/tisseo/demandeur_emploi.py
@@ -0,0 +1,25 @@
+ # -*- coding: utf-8 -*-
+from openfisca_france.model.base import Variable, Individu, MONTH, select
+
+class tisseo_transport_demandeur_emploi_indemnise_reduction(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Pourcentage de la réduction pour les demandeurs d'emploi indemnisés"
+
+    def formula(individu, period, parameters):
+        chomage_net = individu('chomage_net', period.last_month)
+        indemnise = chomage_net > 0
+
+        smic = parameters(period).cotsoc.gen
+        smic_brut_mensuel = smic.smic_h_b * smic.nb_heure_travail_mensuel
+        # Utilisation des valeurs indicatives de service-public.fr pour passer du SMIC brut au SMIC net
+        # https://www.service-public.fr/particuliers/vosdroits/F2300
+        # Dans l'attente de la formule effectivement utilisée par la ville d'Alfortville
+        smic_net_mensuel = 7.82 / 9.88 * smic_brut_mensuel
+
+        cmu_c_plafond = individu.famille('cmu_c_plafond', period) / 12
+        return indemnise * select([
+            chomage_net <= cmu_c_plafond,
+            chomage_net <= smic_net_mensuel
+            ], [100, 80], default=70)
