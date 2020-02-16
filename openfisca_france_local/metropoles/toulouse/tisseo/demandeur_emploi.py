fd335c262edf5ac60af8e526ca029a72e20ec8f3
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-16 17:22:48 +0100

    Ajoute les règles pour les demandeur d'emploi non indemnisés

--- openfisca_france_local/metropoles/toulouse/tisseo/demandeur_emploi.py
+++ openfisca_france_local/metropoles/toulouse/tisseo/demandeur_emploi.py
@@ -23,3 +23,22 @@ class tisseo_transport_demandeur_emploi_indemnise_reduction(Variable):
             chomage_net <= cmu_c_plafond,
             chomage_net <= smic_net_mensuel
             ], [100, 80], default=70)
+
+
+class tisseo_transport_demandeur_emploi_non_indemnise_reduction(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Pourcentage de la réduction pour les demandeurs d'emploi non indemnisés"
+
+    def formula(individu, period, parameters):
+        ressources = individu.foyer_fiscal('rfr', period.n_2) / 12
+
+        smic = parameters(period).cotsoc.gen
+        smic_brut_mensuel = smic.smic_h_b * smic.nb_heure_travail_mensuel
+        # Utilisation des valeurs indicatives de service-public.fr pour passer du SMIC brut au SMIC net
+        # https://www.service-public.fr/particuliers/vosdroits/F2300
+        # Dans l'attente de la formule effectivement utilisée par la ville d'Alfortville
+        smic_net_mensuel = 7.82 / 9.88 * smic_brut_mensuel
+
+        return (ressources <= smic_net_mensuel) * 80
