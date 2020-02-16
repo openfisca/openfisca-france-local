139c66618e60852d5f50fa472b2ef5a456cd3540
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-16 11:46:35 +0100

    Ajoute les règles pour les jeunes

--- openfisca_france_local/metropoles/toulouse/tisseo/jeunes.py
+++ openfisca_france_local/metropoles/toulouse/tisseo/jeunes.py
@@ -1,8 +1,42 @@
  # -*- coding: utf-8 -*-
-from openfisca_france.model.base import Variable, FoyerFiscal, Individu, MONTH, YEAR
+from openfisca_france.model.base import Variable, FoyerFiscal, Individu, MONTH, YEAR, max_
+from openfisca_france.model.prestations.education import TypesScolarite
 
 class tisseo_transport_jeune_reduction(Variable):
     value_type = float
     entity = Individu
     definition_period = MONTH
     label = u"Pourcentage de la réduction pour les jeunes"
+
+    def formula(individu, period):
+        age = individu('age', period)
+
+        return (
+          (4 < age) * ( age < 20) * 80 +
+          (20 <= age) * ( age < 26) * 70
+          )
+
+class tisseo_transport_etudiant_reduction(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Pourcentage de la réduction pour les jeunes"
+
+    def formula(individu, period):
+        age = individu('age', period)
+
+        scolarite = individu('scolarite', period)
+        scolaire = (scolarite == TypesScolarite.lycee) + (scolarite == TypesScolarite.college)
+
+        etudiant = individu('etudiant', period)
+        eligible = scolaire + etudiant
+        aide_non_boursier = (20 <= age) * ( age < 26) * eligible * 80
+
+        boursier = individu('boursier', period)
+        echelon_bourse = individu('echelon_bourse', period)
+        aide_boursier = etudiant * boursier * (echelon_bourse == 7) * (age < 35) * 100
+
+        return max_(
+          aide_boursier,
+          aide_non_boursier
+        )
