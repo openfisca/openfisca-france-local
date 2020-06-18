ebc713b8cc3056d0291b5d630f81986746d14e04
sandcha <sandcha@users.noreply.github.com>
2020-06-18 13:58:18 +0200

    Initialise eure_et_loir_a_recu_adefip et ajoute un 3ème cas réel d'éligibilité

--- openfisca_france_local/departements/eure_et_loir/adefip.py
+++ openfisca_france_local/departements/eure_et_loir/adefip.py
@@ -1,5 +1,13 @@
  # -*- coding: utf-8 -*-
+from numpy import logical_not as not_
+
 from openfisca_france.model.base import Variable, Individu, MONTH
+
+class eure_et_loir_a_recu_adefip(Variable):
+    value_type = bool
+    entity = Individu
+    definition_period = MONTH
+    label = u"A perçu l'AdéFIP dans les 12 derniers mois"
 
 
 class eure_et_loir_eligibilite_adefip(Variable):
@@ -11,5 +19,6 @@ class eure_et_loir_eligibilite_adefip(Variable):
     def formula(individu, period):
         recoit_rsa = individu.famille('rsa', period) > 0
         reside_eure_et_loir = individu.menage('eure_et_loir_eligibilite_residence', period)
+        eure_et_loir_a_recu_adefip = individu('eure_et_loir_a_recu_adefip', period)
 
-        return recoit_rsa * reside_eure_et_loir
+        return not_(eure_et_loir_a_recu_adefip) * recoit_rsa * reside_eure_et_loir
