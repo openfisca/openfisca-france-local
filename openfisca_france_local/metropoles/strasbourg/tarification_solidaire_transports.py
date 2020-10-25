fa32641e835c58ce7c58c18f44e2cdbad0b00000
Thomas Guillet <guillet.thomas@gmail.com>
2020-10-25 11:25:53 +0100

    Ajoute la tarification solidaire des transports pour l'Eurométropole de Strasbourg

---
+++ openfisca_france_local/metropoles/strasbourg/tarification_solidaire_transports.py
@@ -0,0 +1,96 @@
+from openfisca_france.model.base import Variable, Individu, Menage, MONTH, YEAR, select
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_eligibilite_geographique(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+    label = u"Éligibilité géographique pour la tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+    def formula(menage, period):
+        return menage('menage_dans_epci_siren_246700488', period)
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial_de_base(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Quotient familial pour la tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+    def formula(individu, period):
+        return individu.foyer_fiscal('rfr', period.n_2) / 12 / individu.foyer_fiscal('nbptr', period.n_2)
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Quotient familial pour la tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+
+    def formula(individu, period):
+        etudiant = individu('etudiant', period)
+        qf_etudiant = individu('eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial_etudiant', period)
+        base = individu('eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial_de_base', period)
+        return select([etudiant], [qf_etudiant], default=base)
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial_etudiant(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Équivalence du quotient familial d'étudiants pour la tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+
+    def formula(individu, period):
+        etudiant = individu('etudiant', period)
+        boursier = individu('boursier', period)
+        echelon_bourse = individu('echelon_bourse', period)
+        return etudiant * select([boursier],[
+            select(
+            [
+            5 <= echelon_bourse,
+            3 <= echelon_bourse,
+            1 <= echelon_bourse,
+            ],
+            [
+            355,
+            560,
+            765
+            ],
+            default=765)
+          ],
+          default=765)
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_eligible_tarif_reduit(Variable):
+    value_type = bool
+    entity = Individu
+    definition_period = MONTH
+    label = u"Quotient familial pour la tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+    def formula(individu, period):
+        age = individu('age', period)
+        taux_incapacite = individu('taux_incapacite', period)
+        return (26 < age) + (65 <= age) + (0.80 <= taux_incapacite)
+
+
+class eurometropole_strasbourg_tarification_solidaire_transport_montant(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    label = u"Tarif de tarification solidaire des transports de l'Eurométropole de Strasbourg"
+
+    def formula(individu, period):
+        geo = individu.menage('eurometropole_strasbourg_tarification_solidaire_transport_eligibilite_geographique', period)
+        reduit = individu('eurometropole_strasbourg_tarification_solidaire_transport_eligible_tarif_reduit', period)
+        qf = individu('eurometropole_strasbourg_tarification_solidaire_transport_quotient_familial', period)
+        return geo * select(
+            [qf <= 356, qf <= 560, qf < 765],
+            [
+                 5.8 + reduit * ( 3.4 -  5.8),
+                13.4 + reduit * ( 7.3 - 13.4),
+                25.7 + reduit * (13.6 - 25.7),
+            ],
+            default=select([reduit],[27.6], default=0)
+            )
