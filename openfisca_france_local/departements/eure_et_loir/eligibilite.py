dc42e2e04eb98a46a65d0e09ae2cb574b0a1c590
sandcha <sandcha@users.noreply.github.com>
2020-11-06 11:50:43 +0100

    Initialisation de la variable eure_et_loir_eligibilite_repas_foyer avec test

--- openfisca_france_local/departements/eure_et_loir/eligibilite.py
+++ openfisca_france_local/departements/eure_et_loir/eligibilite.py
@@ -1,5 +1,5 @@
  # -*- coding: utf-8 -*-
-from openfisca_france.model.base import Variable, Menage, MONTH
+from openfisca_france.model.base import Variable, Individu, Menage, MONTH
 
 from numpy.core.defchararray import startswith
 
@@ -8,7 +8,20 @@ class eure_et_loir_eligibilite_residence(Variable):
     value_type = bool
     entity = Menage
     definition_period = MONTH
-    label = u"Éligibilité résidentielle d'un ménage aux dipositifs de L'Eure-et-Loir"
+    label = "Éligibilité résidentielle d'un ménage aux dipositifs de L'Eure-et-Loir"
 
     def formula(menage, period):
         return startswith(menage('depcom', period), b'28')
+
+
+class eure_et_loir_eligibilite_repas_foyer(Variable):
+    value_type = bool
+    entity = Individu
+    definition_period = MONTH
+    label = "Éligibilité à l'aide Rapas Foyer de l'Eure-et-Loir"
+    documentation = '''
+    TODO ajouter le descriptif à destination du simulateur
+    '''
+
+    def formula(individu, period):
+        return False
