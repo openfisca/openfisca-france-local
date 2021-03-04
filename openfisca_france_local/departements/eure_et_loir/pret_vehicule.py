7b2030e799c43b77461728b814451dcc84c71053
sandcha <sandcha@users.noreply.github.com>
2021-03-04 22:12:06 +0100

    Renomme par ajout de séparateur entre prêt et véhicule

--- openfisca_france_local/departements/eure_et_loir/pret_vehicule.py
+++ openfisca_france_local/departements/eure_et_loir/pret_vehicule.py
@@ -2,11 +2,11 @@
 from openfisca_france.model.base import Variable, Individu, MONTH
 
 
-class eure_et_loir_eligibilite_pretvehicule(Variable):
+class eure_et_loir_eligibilite_pret_vehicule(Variable):
     value_type = bool
     entity = Individu
     definition_period = MONTH
-    label = "Prêt de véhicule"
+    label = "En Eure-et-Loir, éligibilité au prêt de véhicule"
     reference = "https://github.com/openfisca/openfisca-france-local/wiki/files/departements/eure-et-loir/Reglement_pret_de_vehicules_juin_2019.pdf"
 
     def formula(individu, period):
