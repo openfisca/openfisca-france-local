15a40550003984d26be35da83d101160597babdd
Thomas Guillet <thomas+github@palya.fr>
2021-03-29 20:44:06 +0200

    Ajoute des variables pour calculer le cout de la cantine à l'échelle d'une famille

--- openfisca_france_local/metropoles/strasbourg/cantine.py
+++ openfisca_france_local/metropoles/strasbourg/cantine.py
@@ -1,23 +1,61 @@
-from openfisca_france.model.base import Variable, Individu, MONTH, select, max_
+from openfisca_france.model.base import Variable, Individu, Famille, MONTH, select, max_
 
 
 class strasbourg_metropole_quotient_familial(Variable):
     value_type = float
-    entity = Individu
+    entity = Famille
     definition_period = MONTH
     label = "Quotient familial pour la tarification solidaire de la cantine de l'Eurométropole de Strasbourg"
 
-    def formula(individu, period):
-        return individu.foyer_fiscal('rfr', period.n_2) / 12 / individu.foyer_fiscal('nbptr', period.n_2)
+    def formula(famille, period):
+        return famille.demandeur.foyer_fiscal('rfr', period.n_2) / 12 / famille.demandeur.foyer_fiscal('nbptr', period.n_2)
 
 
 class strasbourg_metropole_tarification_cantine(Variable):
     value_type = float
-    entity = Individu
+    entity = Famille
     definition_period = MONTH
     label = "Quotient familial pour la tarification solidaire de la cantine de l'Eurométropole de Strasbourg"
-    
-    def formula(individu, period, parameters):
-        qf = individu('strasbourg_metropole_quotient_familial', period)
+
+    def formula(famille, period, parameters):
+        qf = famille('strasbourg_metropole_quotient_familial', period)
         tarif = parameters(period).metropoles.strasbourg.tarifs_cantine
         return tarif.calc(max_(0, qf), right=True)
+
+
+class strasbourg_metropole_nombre_repas_cantine(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+
+
+class strasbourg_metropole_cout_cantine(Variable):
+    value_type = float
+    entity = Famille
+    definition_period = MONTH
+
+    def formula(famille, period, parameters):
+        cout_individu = famille.members('strasbourg_metropole_cout_cantine_individu', period)
+        return famille.sum(cout_individu)
+
+
+# class strasbourg_metropole_cout_cantine2(Variable):
+#     value_type = float
+#     entity = Famille
+#     definition_period = MONTH
+
+#     def formula(famille, period, parameters):
+#         tarif = famille('strasbourg_metropole_tarification_cantine')
+#         cout_individu = famille.members('strasbourg_metropole_cout_cantine_individu', period)
+#         return famille.sum(cout_individu * famille.spread(tarif))
+
+
+class strasbourg_metropole_cout_cantine_individu(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+
+    def formula(individu, period, parameters):
+        tarif = individu.famille('strasbourg_metropole_tarification_cantine', period)
+        repas = individu('strasbourg_metropole_nombre_repas_cantine', period)
+        return tarif * repas
