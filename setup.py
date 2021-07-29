44358bf0fcdc6e3a33d99f4abbae01bfb4114809
Thomas Guillet <thomas@palya.fr>
2021-07-29 16:36:59 +0200

    Adapte à France 61.x.x

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires = [
         'OpenFisca-Core >= 25, < 36',
-        'OpenFisca-France >= 58, < 61',
+        'OpenFisca-France >= 61, < 62',
         'pandas == 1.0.3'
         ],
     extras_require = {
