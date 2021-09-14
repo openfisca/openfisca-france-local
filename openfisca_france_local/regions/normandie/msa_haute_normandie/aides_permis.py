66f8a236639bd96d75063260a79b0626b7622b5d
kout95 <antoine.bernier@outlook.fr>
2021-09-14 10:52:54 +0200

    Ajoute l'aide au permis msa haute normandie

---
+++ openfisca_france_local/regions/normandie/msa_haute_normandie/aides_permis.py
@@ -0,0 +1,37 @@
+from numpy.core.defchararray import startswith
+
+from openfisca_france.model.base import Individu, MONTH, Variable
+from openfisca_france.model.caracteristiques_socio_demographiques.demographie import RegimeSecuriteSociale
+
+code_departements = [b'27', b'76']
+
+
+class msa_haute_normandie_aide_permis(Variable):
+    value_type = float
+    entity = Individu
+    label = "Éligibilité financière à l'aide à l’obtention du permis de conduire de la MSA Haute-Normandie"
+    reference = "https://hautenormandie.msa.fr/lfy/aide-au-permis-de-conduire"
+    definition_period = MONTH
+
+    def formula(individu, period, parameters):
+        params = parameters(period).regions.normandie.msa_haute_normandie.aide_permis
+        ars_params = parameters(period).prestations.prestations_familiales.ars
+
+        montant = params.montant
+
+        allocataire_msa = individu('regime_securite_sociale', period) == RegimeSecuriteSociale.regime_agricole
+        enfant_a_charge = individu('enfant_a_charge', period.this_year)
+        alternant = individu('alternant', period)
+
+        depcom = individu.menage('depcom', period)
+        eligibilite_geographique = sum([startswith(depcom, code_departement) for code_departement in code_departements])
+
+        age = individu('age', period)
+        eligibilite_age = (age >= params.age.minimum) * (age <= params.age.maximum)
+
+
+        af_nbenf = individu.famille('af_nbenf', period)
+        plafond_ressources = ars_params.plafond_ressources * (1 + af_nbenf * ars_params.majoration_par_enf_supp)
+        eligibilite_plafond_ressources = individu.foyer_fiscal('rfr', period.this_year) < plafond_ressources
+
+        return (allocataire_msa + (enfant_a_charge * alternant)) * eligibilite_geographique * eligibilite_age * eligibilite_plafond_ressources * montant
