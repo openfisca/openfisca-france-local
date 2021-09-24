d23b750f3b3d51c3579287255b222000406e639e
kout95 <antoine.bernier@outlook.fr>
2021-09-24 11:31:48 +0200

    Prend les 3 derniers salaires

--- openfisca_france_local/regions/bretagne/msa_portes_de_bretagne/aide_permis.py
+++ openfisca_france_local/regions/bretagne/msa_portes_de_bretagne/aide_permis.py
@@ -1,6 +1,6 @@
 from numpy.core.defchararray import startswith
 
-from openfisca_france.model.base import Individu, MONTH, Variable
+from openfisca_france.model.base import Individu, MONTH, Variable, ADD
 from openfisca_france.model.caracteristiques_socio_demographiques.demographie import RegimeSecuriteSociale
 
 code_departements = [b'35', b'56']
@@ -28,7 +28,7 @@ class msa_portes_de_bretagne_aide_permis(Variable):
 
         smic = parameters(period).marche_travail.salaire_minimum
         smic_brut_mensuel = smic.smic_h_b * smic.nb_heure_travail_mensuel
-        eligibilite_salaire_alternant = individu('salaire_de_base', period) / smic_brut_mensuel * 100 <= params.pourcentage_ressources_maximum_alternant
+        eligibilite_salaire_alternant = individu('salaire_de_base', period.last_3_months, options = [ADD]) / 3 / smic_brut_mensuel * 100 <= params.pourcentage_ressources_maximum_alternant
         eligibilite_alternant = individu('alternant', period) * eligibilite_salaire_alternant
 
         quotient_familial = individu.foyer_fiscal('rfr', period.this_year) / 12 / individu.foyer_fiscal('nbptr', period.this_year)
