9ecd90d16ec0eef2cdc4afa415fb5ab76ef9d8e7
Quentin Madura <quentin.madura@gmail.com>
2021-10-12 15:28:45 +0200

    Ajoute l'aide au permis pour la région Auvergne-Rhône-Alpes

---
+++ openfisca_france_local/regions/auvergne_rhone_alpes/aide_permis.py
@@ -0,0 +1,20 @@
+from openfisca_france.model.base import Individu, MONTH, Variable, ADD
+
+class auvergne_rhone_alpes_aide_permis(Variable):
+    value_type = float
+    entity = Individu
+    label = "Éligibilité financière pour l’obtention de l'aide au permis de conduire pour la région Auvergne-Rhône-Alpes."
+    reference = "https://www.auvergnerhonealpes.fr/aide/385/289-financer-ma-formation-au-permis-de-conduire-orientation-formation.htm#thema146"
+    definition_period = MONTH
+
+    def formula(individu, period, parameters):
+        params = parameters(period).regions.auvergne_rhone_alpes.aide_permis
+
+        montant = params.montant
+
+        eligibilite_residentielle = individu.menage("auvergne_rhone_alpes_eligibilite_residence", period)
+
+        age = individu('age', period)
+        eligibilite_age = (age >= params.age.minimum) * (age < params.age.maximum)
+
+        return eligibilite_residentielle * eligibilite_age * montant
