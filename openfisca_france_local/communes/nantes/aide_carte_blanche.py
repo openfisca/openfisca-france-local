14fee3e0f2943b82b320860ba0f21effed91ddbb
Quentin Madura <quentin.madura@gmail.com>
2021-10-12 16:42:41 +0200

    Change la période de référence pour le calcul du quotient familial

--- openfisca_france_local/communes/nantes/aide_carte_blanche.py
+++ openfisca_france_local/communes/nantes/aide_carte_blanche.py
@@ -12,7 +12,7 @@ class aide_carte_blanche(Variable):
     def formula(individu, period, parameters):
 
         eligibilite_commune = individu.menage('depcom', period) == b"44109"
-        quotient_familial = individu.foyer_fiscal('rfr', period.this_year) / 12 / individu.foyer_fiscal('nbptr',
-                                                                                                 period.this_year)
+        quotient_familial = individu.foyer_fiscal('rfr', period.n_2) / 12 / individu.foyer_fiscal('nbptr',
+                                                                                                 period.n_2)
         eligibilite_quotient_familial = quotient_familial <= parameters(period).communes.nantes.aide_carte_blanche.plafond_quotient_familial
         return eligibilite_commune * eligibilite_quotient_familial
