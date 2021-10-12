af365b5e37817fe0bf0fabe008854bedde208abe
Quentin Madura <quentin.madura@gmail.com>
2021-10-12 16:49:07 +0200

    Aère le code de la formule

--- openfisca_france_local/communes/nantes/aide_carte_blanche.py
+++ openfisca_france_local/communes/nantes/aide_carte_blanche.py
@@ -6,12 +6,12 @@ class aide_carte_blanche(Variable):
     entity = Individu
     definition_period = MONTH
     label = "Éligibilité à l'obtention d'une carte blanche donnant accès à de nombreuses offres de loisirs culturelles et sportives à tarif réduit."
-    reference = "https://metropole.nantes.fr/carte-blanche",
+    reference = "https://metropole.nantes.fr/carte-blanche"
 
     def formula(individu, period, parameters):
-
         eligibilite_commune = individu.menage('depcom', period) == b"44109"
-        quotient_familial = individu.foyer_fiscal('rfr', period.n_2) / 12 / individu.foyer_fiscal('nbptr',
-                                                                                                 period.n_2)
+
+        quotient_familial = individu.foyer_fiscal('rfr', period.n_2) / 12 / individu.foyer_fiscal('nbptr', period.n_2)
         eligibilite_quotient_familial = quotient_familial <= parameters(period).communes.nantes.aide_carte_blanche.plafond_quotient_familial
+
         return eligibilite_commune * eligibilite_quotient_familial
