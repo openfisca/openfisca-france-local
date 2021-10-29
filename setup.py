4ed92f80333c3668cfb518696f4deaf1d35f36be
kout95 <antoine.bernier@outlook.fr>
2021-10-29 11:00:57 +0200

    Met à jour la version d'openfisca-france

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="1.12.0",
+    version="1.12.1",
     description="Extension OpenFisca pour nos partenariats avec les collectivités territoriales",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
@@ -11,7 +11,7 @@ setup(
     include_package_data=True,
     install_requires = [
         'OpenFisca-Core >= 25, < 36',
-        'OpenFisca-France >= 77.3, < 80',
+        'OpenFisca-France >= 77.3, < 81',
         'pandas == 1.0.3'
         ],
     extras_require = {
