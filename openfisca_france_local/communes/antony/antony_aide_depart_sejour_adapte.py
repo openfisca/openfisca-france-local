6041241abebe3dda3c4831a3765440cb1349f33b
TOMAS Frederic <frederic.tomas@soprasteria.com>
2021-11-16 09:27:12 +0100

    Ajoute les aides de la ville de Antony

---
+++ openfisca_france_local/communes/antony/antony_aide_depart_sejour_adapte.py
@@ -0,0 +1,46 @@
+ # -*- coding: utf-8 -*-
+from openfisca_france.model.base import *  # noqa analysis:ignore
+
+
+class antony_aide_depart_sejour_adapte(Variable):
+    value_type = float
+    entity = Famille
+    definition_period = MONTH
+    label = "Montant total de l'Aide au depart en sejour adapté de la ville de Antony"
+
+    def formula(famille, period, parameters):
+        residence_antony = famille.demandeur.menage('antony_eligibilite_residence', period)
+
+        condition_ressources_remplies = famille('antony_eligibilite_ressources', period)
+
+        handicap = famille.members('handicap', period)
+        montant_individuel = famille('antony_aide_depart_sejour_adapte_montant_individuel', period)
+        montant_total = famille.sum(montant_individuel * handicap)
+
+        return residence_antony * condition_ressources_remplies * montant_total
+
+
+class antony_aide_depart_sejour_adapte_montant_individuel(Variable):
+    value_type = float
+    entity = Famille
+    definition_period = MONTH
+    label = "Montant individuel de l'Aide au depart en sejour adapté de la ville de Antony"
+
+    def formula(famille, period, parameters):
+        parameters_antony = parameters(period).communes.antony.plafonds_revenus.sejour_adapte
+
+        ressources_famille = famille('antony_base_ressources', period)
+
+        montant = select(
+            [
+                ressources_famille < parameters_antony.tranches.tranche_1,
+                ressources_famille < parameters_antony.tranches.tranche_2
+            ],
+            [
+                parameters_antony.montants.montant_tranche_1,
+                parameters_antony.montants.montant_tranche_2
+            ],
+            default=0
+        )
+
+        return montant
---
+++ openfisca_france_local/communes/antony/antony_bourse_communale.py
@@ -0,0 +1,22 @@
+ # -*- coding: utf-8 -*-
+from openfisca_france.model.base import *  # noqa analysis:ignore
+from openfisca_france.model.prestations.education import TypesScolarite
+
+
+class antony_bourse_communale(Variable):
+    value_type = bool
+    entity = Famille
+    definition_period = MONTH
+    label = "Eligibilité de la famille à la Bourse Communale de la ville de Antony"
+
+    def formula(famille, period, parameters):
+        residence_antony = famille.demandeur.menage('antony_eligibilite_residence', period)
+
+        condition_ressources_remplies = famille('antony_eligibilite_ressources', period)
+
+        scolarite = famille.members('scolarite', period)
+        scolarise = ((scolarite == TypesScolarite.college) + (scolarite == TypesScolarite.lycee))
+
+        au_moins_un_enfant_scolarise = famille.any(scolarise, role = Famille.ENFANT)
+
+        return residence_antony * condition_ressources_remplies * au_moins_un_enfant_scolarise
