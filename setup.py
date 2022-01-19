3c59c26deff0142faf1bd5cc6a3338b18789b50c
kout95 <antoine.bernier@outlook.fr>
2022-01-19 11:40:42 +0100

    Supprime le parallélisme des tests et update le setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,22 @@ from setuptools import setup, find_packages
 setup(
     name="OpenFisca-France-Local",
     version="2.1.0",
+    author="OpenFisca Team",
+    author_email="contact@openfisca.fr",
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
     description="Extension OpenFisca pour nos partenariats avec les collectivités territoriales",
+    keywords="benefit france france-local microsimulation social tax",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    author="",
+    url="https://github.com/openfisca/openfisca-france",
+
     packages=find_packages(),
     include_package_data=True,
     install_requires = [
@@ -22,9 +35,5 @@ setup(
             'xlrd == 1.2.0'
             ]
         },
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-    ],
     scripts=['openfisca_france_local/scripts/openfisca_local_test']
 )
