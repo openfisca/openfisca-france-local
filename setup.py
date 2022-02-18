e47b707aff63aa24de7d8840166f4dfc50fe04e3
kout95 <antoine.bernier@outlook.fr>
2022-02-18 09:48:10 +0100

    Augmente la version dans le fichier 'setup.py'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="3.0.0",
+    version="4.0.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers = [
