5489be44ddeef2ebecd9d3328fd682f7280df00b
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-11-04 17:27:49 +0100

    Reforme aide automatique: chargement fichier aide
    	- Nous chargeons maintenant directement un fichier yaml

--- openfisca_france_local/aides_jeunes_reform.py
+++ openfisca_france_local/aides_jeunes_reform.py
@@ -8,17 +8,6 @@ from openfisca_core import reforms
 
 import yaml
 
-yaml_content = """slug: etat-aide-nationale-exceptionnelle-au-brevet-daptitude-aux-fonctions-danimateur-bafa
-conditions_generales:
-  - type: age
-    operator: ">="
-    value: 17
-type: float
-periodicite: ponctuelle
-montant: 200
-"""
-aide = yaml.safe_load(yaml_content)
-
 
 def generate_variable(benefit):
 
@@ -37,12 +26,15 @@ def generate_variable(benefit):
             # eligibility = startswith(individu.menage('depcom', period), condition["values"][0])
             return amount * eligibility
 
-    NewAidesJeunesBenefitVariable.__name__ = benefit['slug'].replace('-', '_')
+    NewAidesJeunesBenefitVariable.__name__ = benefit['slug']
     return NewAidesJeunesBenefitVariable
 
 
 class aides_jeunes_reform_dynamic(reforms.Reform):
 
     def apply(self):
-
-        self.add_variable(generate_variable(aide))
+        benefit_file_path = '../git_aides-jeunes/data/benefits/javascript/etat-aide-nationale-exceptionnelle-au-brevet-daptitude-aux-fonctions-danimateur-bafa.yml'
+        benefit: dict = yaml.safe_load(open(benefit_file_path))
+        benefit['slug'] = benefit_file_path.split(
+            '/')[-1].replace('-', '_').split('.')[0]
+        self.add_variable(generate_variable(benefit))
