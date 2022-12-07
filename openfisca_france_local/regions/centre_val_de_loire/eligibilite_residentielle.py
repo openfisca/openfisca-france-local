db41019b89c91fd6520255d7455458136fcdd8d5
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-07 16:09:53 +0100

    Gestion regions, test CVDL: vert

---
+++ openfisca_france_local/regions/centre_val_de_loire/eligibilite_residentielle.py
@@ -0,0 +1,17 @@
+from openfisca_france.model.base import Variable, Menage, MONTH
+from openfisca_core.populations.population import Population
+from openfisca_core.periods import Period
+from numpy.core.defchararray import startswith
+
+DEPARTEMENTS_CENTRE_VAL_DE_LOIRE = [b'18', b'28', b'36', b'37', b'41', b'45']
+
+
+class centre_val_de_loire_eligibilite_residence(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+    label = "Éligibilité résidentielle d'un ménage aux dipositifs de la région centre val de loire"
+
+    def formula(menage: Population, period: Period):
+        depcom = menage('depcom', period)
+        return sum([startswith(depcom, code_departement) for code_departement in DEPARTEMENTS_CENTRE_VAL_DE_LOIRE]) > 0
