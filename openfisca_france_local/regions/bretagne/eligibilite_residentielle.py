5e6209e1993bbc7e942ad0b386b4bb99ec5fb04d
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-07 17:05:56 +0100

    Ajout eligibilité Bretagne : vert

---
+++ openfisca_france_local/regions/bretagne/eligibilite_residentielle.py
@@ -0,0 +1,18 @@
+from openfisca_france.model.base import Variable, Menage, MONTH
+from openfisca_core.populations.population import Population
+from openfisca_core.periods import Period
+from numpy.core.records import array as np_array
+from numpy.core.defchararray import startswith
+
+DEPARTEMENTS_BRETAGNE = [b'22', b'29', b'35', b'56']
+
+
+class bretagne_eligibilite_residence(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+    label = "Éligibilité résidentielle d'un ménage aux dipositifs de la région Bretagne"
+
+    def formula(menage: Population, period: Period) -> np_array:
+        depcom: np_array = menage('depcom', period)
+        return sum([startswith(depcom, code_departement) for code_departement in DEPARTEMENTS_BRETAGNE]) > 0
