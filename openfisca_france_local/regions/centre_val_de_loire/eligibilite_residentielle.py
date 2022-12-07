d477aec3c374e79e2edf4545b6e026a7f71bbc4d
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-07 16:13:15 +0100

    Refacto typing
    	- Ajout de typage pour une meilleur lecture

--- openfisca_france_local/regions/centre_val_de_loire/eligibilite_residentielle.py
+++ openfisca_france_local/regions/centre_val_de_loire/eligibilite_residentielle.py
@@ -1,6 +1,7 @@
 from openfisca_france.model.base import Variable, Menage, MONTH
 from openfisca_core.populations.population import Population
 from openfisca_core.periods import Period
+from numpy.core.records import array as np_array
 from numpy.core.defchararray import startswith
 
 DEPARTEMENTS_CENTRE_VAL_DE_LOIRE = [b'18', b'28', b'36', b'37', b'41', b'45']
@@ -12,6 +13,6 @@ class centre_val_de_loire_eligibilite_residence(Variable):
     definition_period = MONTH
     label = "Éligibilité résidentielle d'un ménage aux dipositifs de la région centre val de loire"
 
-    def formula(menage: Population, period: Period):
-        depcom = menage('depcom', period)
+    def formula(menage: Population, period: Period) -> np_array:
+        depcom: np_array = menage('depcom', period)
         return sum([startswith(depcom, code_departement) for code_departement in DEPARTEMENTS_CENTRE_VAL_DE_LOIRE]) > 0
