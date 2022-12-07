7a3d4cd4d9d42e1258a144bf1ccaa63c0c2743cd
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-07 16:09:53 +0100

    Gestion regions, test CVDL: refacto : vert

--- openfisca_france_local/aides_jeunes_reform.py
+++ openfisca_france_local/aides_jeunes_reform.py
@@ -39,6 +39,7 @@ def is_department_eligible(individu: Population, period: Period, condition):
 def is_region_eligible(individu: Population, period: Period, condition):
     eligibilite_region_table: dict = {
         '01': 'guadeloupe_eligibilite_residence',
+        '24': 'centre_val_de_loire_eligibilite_residence',
         '32': 'hauts_de_france_eligibilite_residence',
         '76': 'occitanie_eligibilite_residence',
         '84': 'auvergne_rhone_alpes_eligibilite_residence',
