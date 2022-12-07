9155ba7b2dc804881579e379411fdabfab75c207
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-07 16:40:42 +0100

    Gestion regions, test Grand Est: refacto : vert

---
+++ openfisca_france_local/regions/grand_est/eligibilite_residentielle.py
@@ -0,0 +1,16 @@
+from openfisca_france.model.base import Variable, Menage, MONTH
+from openfisca_core.populations.population import Population
+from openfisca_core.periods import Period
+from numpy.core.records import array as np_array
+from numpy.core.defchararray import startswith
+
+
+class grand_est_eligibilite_residence(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+    label = "Éligibilité résidentielle d'un ménage aux dipositifs de la région Grand Est"
+
+    def formula(menage: Population, period: Period, parameters) -> np_array:
+        depcom: np_array = menage('depcom', period)
+        return sum([startswith(depcom, str.encode(code)) for code in parameters(period).regions.grand_est.departements]) > 0
