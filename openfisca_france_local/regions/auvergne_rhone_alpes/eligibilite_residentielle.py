45b7a8d9e9f2bd5237a24ceab4e52e4e2266e8b8
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-08 09:16:15 +0100

    Ajout test pour le département isère : vert

--- openfisca_france_local/regions/auvergne_rhone_alpes/eligibilite_residentielle.py
+++ openfisca_france_local/regions/auvergne_rhone_alpes/eligibilite_residentielle.py
@@ -2,7 +2,10 @@ from openfisca_france.model.base import Variable, Menage, MONTH
 
 from numpy.core.defchararray import startswith
 
-DEPARTEMENT_AUVERGNE_RHONE_ALPES = [b'01', b'03', b'07', b'15', b'26', b'42', b'43', b'63', b'69', b'73', b'74']
+DEPARTEMENT_AUVERGNE_RHONE_ALPES = [
+    b'01', b'03', b'07', b'15', b'26', b'38', b'42', b'43', b'63', b'69', b'73', b'74'
+]
+
 
 class auvergne_rhone_alpes_eligibilite_residence(Variable):
     value_type = bool
