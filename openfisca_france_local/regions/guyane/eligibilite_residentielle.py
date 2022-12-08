a49a191c7f9f476ae614db49a74b34f94c502b2a
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-08 15:06:52 +0100

    Ajouts test eligibilité Guyane : vert

---
+++ openfisca_france_local/regions/guyane/eligibilite_residentielle.py
@@ -0,0 +1,20 @@
+from openfisca_france.model.base import Variable, Menage, MONTH
+from openfisca_core.populations.population import Population
+from openfisca_core.periods import Period
+from numpy.core.records import array as np_array
+from numpy.core.defchararray import startswith
+
+DEPARTEMENT_GUYANE = [
+    b'973',
+]
+
+
+class guyane_eligibilite_residence(Variable):
+    value_type = bool
+    entity = Menage
+    definition_period = MONTH
+    label = "Éligibilité résidentielle d'un ménage aux dipositifs de la région Guyane"
+
+    def formula(menage: Population, period: Period) -> np_array:
+        depcom: np_array = menage('depcom', period)
+        return sum([startswith(depcom, code_departement) for code_departement in DEPARTEMENT_GUYANE]) > 0
