c1bf44dd3061dc3fd60458960b66e2fee182a254
Allan - CodeWorks <allan.mamy@codeworks.fr>
2022-12-19 11:29:28 +0100

    Incrément de version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="4.2.1",
+    version="4.3.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers = [
