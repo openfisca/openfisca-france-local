412459078f288300a76f399b578d836d17124557
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-01-17 14:58:00 +0100

    Test creation noeud aide : rouge
    	- Teste pour vérifier qu'une aide "aide-x" génèrera un tax and
    benefit system avec un noeud "aide_x"

--- test_reform_parameters.py
+++ test_reform_parameters.py
@@ -1,7 +1,26 @@
+import yaml
 from openfisca_france import CountryTaxBenefitSystem
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
+
+
+def extract_benefit_file_content(benefit_path) -> dict:
+    benefit: dict = yaml.safe_load(open(benefit_path))
+    benefit['slug'] = benefit_path.split(
+        '/')[-1].replace('-', '_').split('.')[0]
+    return benefit
 
 
 def test_dummy():
     tax_benefit_system: TaxBenefitSystem = CountryTaxBenefitSystem()
     assert tax_benefit_system.parameters.marche_travail.prime_pepa.plafond_salaire
+
+
+def test_create_benefit_parameter_node():
+    tax_benefit_system: TaxBenefitSystem = CountryTaxBenefitSystem()
+    benefit_path = "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml"
+    benefit = extract_benefit_file_content(benefit_path)
+
+    new_parameter_node = create_benefit_parameters(benefit)
+    tax_benefit_system.parameters.merge(new_parameter_node)
+
+    assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification
