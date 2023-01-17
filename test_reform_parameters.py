5d0e093dc05ee207c5d58bb74daa377e51534890
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-01-17 16:37:08 +0100

    Test creation noeud age maxi : rouge

--- test_reform_parameters.py
+++ test_reform_parameters.py
@@ -42,3 +42,14 @@ def test_create_age_parameter_node(tax_benefit_system):
         new_parameter_node.name, new_parameter_node)
 
     assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age
+
+
+def test_create_age_maximum_parameter_node(tax_benefit_system):
+    benefit_path = "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml"
+    benefit = extract_benefit_file_content(benefit_path)
+
+    new_parameter_node = create_benefit_parameters(benefit)
+    tax_benefit_system.parameters.add_child(
+        new_parameter_node.name, new_parameter_node)
+
+    assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age.maximum
