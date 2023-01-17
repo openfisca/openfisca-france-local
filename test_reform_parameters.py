e3619821b5d2d62329f45bdfafdcea6c940d0e67
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-01-17 14:41:57 +0100

    Dummy test vert

--- test_reform_parameters.py
+++ test_reform_parameters.py
@@ -4,4 +4,4 @@ from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
 def test_dummy():
     tax_benefit_system: TaxBenefitSystem = CountryTaxBenefitSystem()
-    assert tax_benefit_system.parameters.marche_travail.prime_pepa.parametre_inexistant
+    assert tax_benefit_system.parameters.marche_travail.prime_pepa.plafond_salaire
