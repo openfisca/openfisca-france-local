3793b747ca98a98bacf2a327d64c1d1af2c2c5c1
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-01-18 14:09:50 +0100

    Test creation value : refacto

--- test_reform_parameters.py
+++ test_reform_parameters.py
@@ -7,7 +7,7 @@ from condition_to_parameter import create_benefit_parameters
 
 
 @pytest.fixture
-def tax_benefit_system() -> ParameterNode:
+def parameters() -> ParameterNode:
     return CountryTaxBenefitSystem().parameters
 
 
@@ -18,51 +18,51 @@ def extract_benefit_file_content(benefit_path) -> dict:
     return benefit
 
 
-def test_create_benefit_parameter_node(tax_benefit_system):
+def test_create_benefit_parameter_node(parameters):
     benefit_path = "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml"
     benefit = extract_benefit_file_content(benefit_path)
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification
+    assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification
 
 
-def test_create_age_parameter_node(tax_benefit_system):
+def test_create_age_parameter_node(parameters):
     benefit_path = "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml"
     benefit = extract_benefit_file_content(benefit_path)
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age
+    assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age
 
 
-def test_create_age_maximum_parameter_node(tax_benefit_system):
+def test_create_age_maximum_parameter_node(parameters):
     benefit_path = "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml"
     benefit = extract_benefit_file_content(benefit_path)
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    assert tax_benefit_system.parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age.maximum
+    assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age.maximum
 
 
-def test_create_age_strictement_inferieur_parameter_node(tax_benefit_system):
+def test_create_age_strictement_inferieur_parameter_node(parameters):
     benefit_path = "benefits/departement-val-d-oise-bourse-aux-apprentis.yml"
     benefit = extract_benefit_file_content(benefit_path)
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    assert tax_benefit_system.parameters.departement_val_d_oise_bourse_aux_apprentis.age.strictement_inferieur
+    assert parameters.departement_val_d_oise_bourse_aux_apprentis.age.strictement_inferieur
 
 
-def test_create_age_strictement_superieur_parameter_node(tax_benefit_system):
+def test_create_age_strictement_superieur_parameter_node(parameters):
 
     benefit: dict = {
         "slug": "inf_parameter",
@@ -70,21 +70,21 @@ def test_create_age_strictement_superieur_parameter_node(tax_benefit_system):
     }
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    assert tax_benefit_system.parameters.inf_parameter.age.strictement_superieur
+    assert parameters.inf_parameter.age.strictement_superieur
 
 
-def test_create_age_value_parameter_node(tax_benefit_system):
+def test_create_age_value_parameter_node(parameters):
     benefit_path = "benefits/departement-val-d-oise-bourse-aux-apprentis.yml"
     benefit = extract_benefit_file_content(benefit_path)
 
     new_parameter_node = create_benefit_parameters(benefit)
-    tax_benefit_system.parameters.add_child(
+    parameters.add_child(
         new_parameter_node.name, new_parameter_node)
 
-    at_instant = tax_benefit_system.parameters("2023-01-01")
+    at_instant = parameters("2023-01-01")
     parameter = at_instant.departement_val_d_oise_bourse_aux_apprentis
 
     assert parameter.age.strictement_inferieur == 25
