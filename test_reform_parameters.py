e937557673453f6751cff18dfeba321236e430e7
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-02-06 16:02:08 +0100

    Copie des fichiers necessaires pour les tests
    	- renommage de l'extension pour suivre les recommandations du
    maintenaneur de YAML

--- test_reform_parameters.py
+++ test_reform_parameters.py
@@ -30,28 +30,28 @@ def extract_benefit_file_content(benefit_path) -> dict:
 
 def test_create_benefit_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml")
+                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yaml")
 
     assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification
 
 
 def test_create_age_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml")
+                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yaml")
 
     assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age
 
 
 def test_create_age_maximum_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml")
+                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yaml")
 
     assert parameters.caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age.maximum
 
 
 def test_create_age_maximum_parameter_with_value(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yml")
+                              "benefits/caf_oise-aide-au-bafa-pour-une-session-de-formation-dapprofondissement-ou-de-qualification.yaml")
 
     assert parameters(str(date.today(
     ))).caf_oise_aide_au_bafa_pour_une_session_de_formation_dapprofondissement_ou_de_qualification.age.maximum == 16
@@ -59,7 +59,7 @@ def test_create_age_maximum_parameter_with_value(parameters):
 
 def test_create_both_age_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_pas_de_calais-aide-au-bafa-pour-une-session-de-formation-générale.yml")
+                              "benefits/caf_pas_de_calais-aide-au-bafa-pour-une-session-de-formation-générale.yaml")
 
     benefit_parameter = parameters(str(date.today(
     ))).caf_pas_de_calais_aide_au_bafa_pour_une_session_de_formation_générale
@@ -70,7 +70,7 @@ def test_create_both_age_parameter_node(parameters):
 
 def test_create_age_strictement_inferieur_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/departement-val-d-oise-bourse-aux-apprentis.yml")
+                              "benefits/departement-val-d-oise-bourse-aux-apprentis.yaml")
 
     benefit_parameter = parameters(str(date.today(
     ))).departement_val_d_oise_bourse_aux_apprentis.age.strictement_inferieur
@@ -94,7 +94,7 @@ def test_create_age_strictement_superieur_parameter_node(parameters):
 
 def test_create_age_value_parameter_node(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/departement-val-d-oise-bourse-aux-apprentis.yml")
+                              "benefits/departement-val-d-oise-bourse-aux-apprentis.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.departement_val_d_oise_bourse_aux_apprentis
@@ -104,7 +104,7 @@ def test_create_age_value_parameter_node(parameters):
 
 def test_create_quotient_familial_parameter(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_pas_de_calais-aide-au-bafa-pour-une-session-de-formation-générale.yml")
+                              "benefits/caf_pas_de_calais-aide-au-bafa-pour-une-session-de-formation-générale.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.caf_pas_de_calais_aide_au_bafa_pour_une_session_de_formation_générale
@@ -114,7 +114,7 @@ def test_create_quotient_familial_parameter(parameters):
 
 def test_create_region_parameter(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/hauts-de-france-carte-generation-apprentis-aide-transport.yml")
+                              "benefits/hauts-de-france-carte-generation-apprentis-aide-transport.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.hauts_de_france_carte_generation_apprentis_aide_transport
@@ -124,7 +124,7 @@ def test_create_region_parameter(parameters):
 
 def test_create_regime_securite_sociale_parameter(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf_morbihan-aide-au-brevet-daptitude-aux-fonctions-de-directeur-bafd.yml")
+                              "benefits/caf_morbihan-aide-au-brevet-daptitude-aux-fonctions-de-directeur-bafd.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.caf_morbihan_aide_au_brevet_daptitude_aux_fonctions_de_directeur_bafd
@@ -134,7 +134,7 @@ def test_create_regime_securite_sociale_parameter(parameters):
 
 def test_create_regime_securite_sociale_excludes_parameter(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/caf-val-de-marne-aide-bafa-approfondissement-qualification.yml")
+                              "benefits/caf-val-de-marne-aide-bafa-approfondissement-qualification.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.caf_val_de_marne_aide_bafa_approfondissement_qualification
@@ -144,7 +144,7 @@ def test_create_regime_securite_sociale_excludes_parameter(parameters):
 
 def test_create_formation_sanitaire_social_parameter(parameters):
     generate_parameter_in_TBS(parameters,
-                              "benefits/guadeloupe-bourse-sanitaire.yml")
+                              "benefits/guadeloupe-bourse-sanitaire.yaml")
 
     at_instant = parameters("2023-01-01")
     parameter = at_instant.guadeloupe_bourse_sanitaire
