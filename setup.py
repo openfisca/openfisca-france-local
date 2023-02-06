b0431a706637fe27478bed2797ce7577104c8454
Shamzic <shamery@free.fr>
2023-02-06 11:53:19 +0100

    Incrémente le numéro de version du répo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="4.2.4",
+    version="4.2.5",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers = [
