fc68d85943ff6b260ab9eeef5b55e14d0b11a9b5
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-02-15 19:02:12 +0100

    Mise à joru version dans setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="4.2.8",
+    version="4.2.9",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers = [
@@ -14,7 +14,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Information Analysis",
-        ],
+    ],
     description="Extension OpenFisca pour nos partenariats avec les collectivités territoriales",
     keywords="benefit france france-local microsimulation social tax",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
@@ -22,18 +22,18 @@ setup(
 
     packages=find_packages(),
     include_package_data=True,
-    install_requires = [
-        'OpenFisca-Core >= 35.8.0, < 36',
-        'OpenFisca-France >= 139.0.0, < 143',
+    install_requires=[
+        'OpenFisca-Core >= 35.2.0, < 36',
+        'OpenFisca-France >= 126, < 143',
         'pandas == 1.0.3'
-        ],
-    extras_require = {
+    ],
+    extras_require={
         'test': [
             'nose',
-            ],
+        ],
         'excel-reader': [
             'xlrd == 1.2.0'
-            ]
-        },
+        ]
+    },
     scripts=['openfisca_france_local/scripts/openfisca_local_test']
 )
