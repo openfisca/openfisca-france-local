799e82e882202b1e704ee49c5330bd11f713a7c1
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-02-24 15:32:50 +0100

    Supresison modification hors-sujet

--- openfisca_france_local/regions/occitanie/eligibilite_residentielle.py
+++ openfisca_france_local/regions/occitanie/eligibilite_residentielle.py
@@ -2,9 +2,8 @@ from openfisca_france.model.base import Variable, Menage, MONTH
 from numpy.core.defchararray import startswith
 
 DEPARTEMENTS_OCCITANIE = [
-    b'09', b'11', b'12', b'30', b'31', b'32', b'34', b'46', b'48', b'65', b'66', b'81', b'82'
+    b"09", b"11", b"12", b"30", b"31", b"32", b"34", b"46", b"48", b"65", b"66", b"81", b"82"
 ]
-
 
 class occitanie_eligibilite_residence(Variable):
     value_type = bool
@@ -15,3 +14,4 @@ class occitanie_eligibilite_residence(Variable):
     def formula(menage, period):
         depcom = menage('depcom', period)
         return sum([startswith(depcom, code_departement) for code_departement in DEPARTEMENTS_OCCITANIE]) > 0
+
