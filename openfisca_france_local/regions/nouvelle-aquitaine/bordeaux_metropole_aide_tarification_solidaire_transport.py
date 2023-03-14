5f3616f356e88a5d5dccdddf97bd019cbfdc09af
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-03-14 16:28:44 +0100

    Test QF <= 942 : vert

--- openfisca_france_local/regions/nouvelle-aquitaine/bordeaux_metropole_aide_tarification_solidaire_transport.py
+++ openfisca_france_local/regions/nouvelle-aquitaine/bordeaux_metropole_aide_tarification_solidaire_transport.py
@@ -1,8 +1,8 @@
-from openfisca_france.model.base import Variable, MONTH, Famille
+from openfisca_france.model.base import Variable, MONTH, Individu
 
 
 class bordeaux_metropole_aide_tarification_solidaire_transport(Variable):
-    entity = Famille
+    entity = Individu
     definition_period = MONTH
     value_type = float
     label = "Éligibilité à l'aide tarification solidaire le la métropole de Bordeaux"
@@ -10,4 +10,9 @@ class bordeaux_metropole_aide_tarification_solidaire_transport(Variable):
         'https://tarificationsolidaire.bordeaux-metropole.fr/Accueil.aspx']
 
     def formula(individu, period, parameters):
-        return False
+        rfr = individu.foyer_fiscal('rfr', period.this_year)
+        nbptr = individu.foyer_fiscal('nbptr', period.this_year)
+        qf = rfr / 12 / nbptr
+
+        montant = 30
+        return (qf <= 942) * montant
