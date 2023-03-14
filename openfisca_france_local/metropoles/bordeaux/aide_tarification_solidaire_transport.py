7ec26849daf4bd855e5d63ad250bf232fcb13b32
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-03-14 16:47:57 +0100

    Test QF <= 942 : refacto : ajout parameters

--- openfisca_france_local/metropoles/bordeaux/aide_tarification_solidaire_transport.py
+++ openfisca_france_local/metropoles/bordeaux/aide_tarification_solidaire_transport.py
@@ -10,9 +10,13 @@ class bordeaux_metropole_aide_tarification_solidaire_transport(Variable):
         'https://tarificationsolidaire.bordeaux-metropole.fr/Accueil.aspx']
 
     def formula(individu, period, parameters):
+        benefit_parameters = parameters(
+            period).metropoles.bordeaux.aide_tarification_solidaire_transport
+
         rfr = individu.foyer_fiscal('rfr', period.this_year)
         nbptr = individu.foyer_fiscal('nbptr', period.this_year)
         qf = rfr / 12 / nbptr
 
-        montant = 30
-        return (qf <= 942) * montant
+        montant = benefit_parameters.tranche3.taux_de_reduction
+        max_eligible_qf = benefit_parameters.tranche3.quotient_familial_maximum
+        return (qf <= max_eligible_qf) * montant
