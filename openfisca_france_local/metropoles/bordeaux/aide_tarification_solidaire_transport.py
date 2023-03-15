777124b5b6887a4eb48f270387079dd1fae6362b
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-03-15 14:16:41 +0100

    Ajout tranche 1 : vert

--- openfisca_france_local/metropoles/bordeaux/aide_tarification_solidaire_transport.py
+++ openfisca_france_local/metropoles/bordeaux/aide_tarification_solidaire_transport.py
@@ -18,11 +18,13 @@ class bordeaux_metropole_aide_tarification_solidaire_transport(Variable):
         nbptr = individu.foyer_fiscal('nbptr', period.this_year)
         qf = rfr / 12 / nbptr
 
+        qf_max1 = benefit_parameters.tranche1.quotient_familial_maximum
+        taux1 = benefit_parameters.tranche1.taux_de_reduction
         qf_max2 = benefit_parameters.tranche2.quotient_familial_maximum
         taux2 = benefit_parameters.tranche2.taux_de_reduction
         qf_max3 = benefit_parameters.tranche3.quotient_familial_maximum
         taux3 = benefit_parameters.tranche3.taux_de_reduction
 
-        montant = select([qf <= qf_max2, qf <= qf_max3,
-                         qf > qf_max2], [taux2, taux3, 0])
+        montant = select([qf <= qf_max1, qf <= qf_max2, qf <= qf_max3,
+                         qf > qf_max2], [taux1, taux2, taux3, 0])
         return montant
