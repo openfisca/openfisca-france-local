05b44775f6cb5df0fc187855f444d15545b9e347
Allan-CodeWorks <101192539+Allan-CodeWorks@users.noreply.github.com>
2023-03-23 10:17:40 +0100

    Ajout de l'aide tarification solidaire de Transport Bordeaux Metropole  (#147)
    Ajoute l'aide tarification solidaire de Transport Bordeaux Metropole

---
+++ openfisca_france_local/metropoles/bordeaux/aide_tarification_solidaire_transport.py
@@ -0,0 +1,20 @@
+from openfisca_france.model.base import Variable, MONTH, Individu
+
+
+class bordeaux_metropole_aide_tarification_solidaire_transport(Variable):
+    entity = Individu
+    definition_period = MONTH
+    value_type = float
+    label = "Éligibilité à l'aide tarification solidaire le la métropole de Bordeaux"
+    reference = [
+        'https://tarificationsolidaire.bordeaux-metropole.fr/Accueil.aspx']
+
+    def formula(individu, period, parameters):
+        benefit_parameters = parameters(
+            period).metropoles.bordeaux.aide_tarification_solidaire_transport
+
+        rfr = individu.foyer_fiscal('rfr', period.this_year)
+        nbptr = individu.foyer_fiscal('nbptr', period.this_year)
+        quotient_familial = rfr / 12 / nbptr
+
+        return benefit_parameters['tranches_taux_de_reduction_par_quotient_familial'].calc(quotient_familial)
