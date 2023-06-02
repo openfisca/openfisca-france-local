b0382de76b86f9c909c4d2665b84df0b3f1af98b
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-06-02 15:06:10 +0200

    Ajout du fichier convert_benefit_conditions_to_parameters.py
    	- Contient le code qui prend en entré un fichier d'aide au
    format YAML et donne un ParameterNode contenant les valeurs relatives à
    une aide.

---
+++ openfisca_france_local/convert_benefit_conditions_to_parameters.py
@@ -0,0 +1,141 @@
+from openfisca_france.model.base import (ParameterNode, Parameter)
+
+
+def condition_to_parameter(condition: dict) -> dict:
+
+    def generate_age_parameter(condition: dict) -> dict:
+        parameter_operator: str = comparison_operators[condition["operator"]]
+        data: dict = {
+            condition_type: {
+                parameter_operator: {
+                    "values": {date: {
+                        "value": condition["value"]}},
+                    }
+                }
+            }
+        return data
+
+    def generate_quotient_familial_parameter(condition: dict) -> dict:
+        parameter_operator: str = comparison_operators[condition["operator"]]
+
+        data: dict = {
+            condition_type: {
+                condition["period"]: {
+                    parameter_operator: {
+                        "values": {date: {
+                            "value": condition["value"]
+                            }}
+                        }
+                    }
+                }
+            }
+        return data
+
+    def generate_regime_securite_sociale_parameter(
+            condition: dict) -> ParameterNode:
+        def create_regime_dict(constraint):
+            return {
+                constraint: {
+                    "values": {
+                        date: {"value": condition[constraint]}}
+                    }}
+
+        if "includes" in condition.keys() and "excludes" in condition.keys():
+            raise NotImplementedError(
+                'Condition "regime_securite_sociale" does not support having'
+                ' both "includes" and "excludes" properties.\n'
+                'Please check the YAML file.'
+                )
+
+        constraint: str = "includes" if "includes" in condition.keys() else "excludes"
+
+        data: dict = {condition_type: create_regime_dict(constraint)}
+
+        return data
+
+    def generate_simple_parameter(condition: dict) -> Parameter:
+        if len(condition) == 1:
+            value = {"value": True}
+        else:
+            value = {"value": condition["values"]}
+        data: dict = {
+            condition_type: {
+                "values": {date: value}
+                }
+            }
+        return data
+
+    condition_table: dict = {
+        "age": generate_age_parameter,
+        "quotient_familial": generate_quotient_familial_parameter,
+        "regime_securite_sociale": generate_regime_securite_sociale_parameter,
+        }
+
+    date = "2020-01-01"
+
+    comparison_operators = {
+        '<=': "maximum",
+        '>=': "minimum",
+        '<': "strictement_inferieur",
+        '>': "strictement_superieur",
+        }
+
+    condition_type: str = condition["type"]
+
+    if condition_type in condition_table.keys():
+        condition_parameter = condition_table[condition_type](condition)
+    else:
+        condition_parameter = generate_simple_parameter(condition)
+    return condition_parameter
+
+
+def conditions_to_node_data(conditions: "list[dict]") -> dict:
+    conditions_formated: "list[dict]" = [condition_to_parameter(
+        condition) for condition in conditions]
+    data: dict = {"conditions": {}}
+    for condition in conditions_formated:
+        for key in condition.keys():
+            if key in data["conditions"]:
+                data["conditions"][key].update(condition[key])
+            else:
+                data["conditions"].update(condition)
+
+    return data
+
+
+def profils_to_node_data(profils):
+    date = "2020-01-01"
+
+    profil_dict: dict = {'profils': {}}
+    for profil in profils:
+        profil_dict['profils'].update({profil['type']: {}})
+        if 'conditions' in profil:
+            profil_dict['profils'][profil['type']].update(
+                conditions_to_node_data(profil["conditions"]))
+        else:
+            profil_dict['profils'][profil['type']] = {date: {"value": True}}
+    return profil_dict
+
+
+def amount_to_node_data(montant: dict):
+    date = "2020-01-01"
+    return {'montant': {date: {'value': montant}}} if montant else {}
+
+
+def convert_benefit_conditions_to_parameters(benefit: dict) -> ParameterNode:
+    meta_donnees = amount_to_node_data(benefit.get('montant'))
+
+    conditions_generales = benefit['conditions_generales']
+    conditions_generales_dict = conditions_to_node_data(
+        conditions_generales)
+
+    node_data: dict = {}
+    node_data.update(conditions_generales_dict)
+    node_data.update(meta_donnees)
+
+    profils: list[dict] = benefit.get("profils")
+    if profils:
+        profil_dict = profils_to_node_data(profils)
+        node_data.update(profil_dict)
+
+    return ParameterNode(benefit["slug"], data=node_data)
