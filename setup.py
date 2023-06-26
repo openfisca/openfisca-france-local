8ecaac007ddc7d2adfbd2308dab201dc941e6e1c
Allan-CodeWorks <101192539+Allan-CodeWorks@users.noreply.github.com>
2023-06-26 08:53:34 +0200

    Modélise l'aide au permis de conduire du département de la Haute vienne (#163)
    Utilise la source : 
    https://www.haute-vienne.fr/nos-actions/jeunesse/besoin-dun-coup-de-pouce/aide-au-permis-de-conduire
    Certaines données proviennent du PDF à télécharger sur la page source.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="OpenFisca-France-Local",
-    version="5.0.1",
+    version="5.1.0",
     author="OpenFisca Team",
     author_email="contact@openfisca.fr",
     classifiers=[
