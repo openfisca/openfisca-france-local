051270b8308512de64fc531c397bcbdfefab5eae
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-07-07 17:37:16 +0200

    Corrige le crash lié à un mauvais calcul de vecteur

--- openfisca_france_local/regions/hauts_de_france/aide_garde_enfant.py
+++ openfisca_france_local/regions/hauts_de_france/aide_garde_enfant.py
@@ -14,16 +14,19 @@ class hauts_de_france_aide_garde_enfant(Variable):
     definition_period = MONTH
 
     def formula(famille, period, parameters):
+
         modalites = parameters(
             period).regions.hauts_de_france.aide_garde_enfant
         couple = famille('en_couple', period)
-        montant = (modalites.montant.famille_monoparentale * (1 - couple)
-                   ) + (modalites.montant.famille_biparentale * couple)
+
+        montant = ((modalites.montant.famille_monoparentale * (1 - couple)
+                    ) + (modalites.montant.famille_biparentale * couple))
 
         age = famille.members('age', period)
+
         enfants_eligibles = (age < modalites.age_maximum_enfant)
-        montant_par_enfant = montant * enfants_eligibles
-        montant_total = famille.sum(montant_par_enfant, role=Famille.ENFANT)
+
+        montant_total = montant * famille.sum(enfants_eligibles, role=Famille.ENFANT)
 
         region = famille.demandeur.menage('region', period)
         eligibilite_geographique = sum(
@@ -33,7 +36,6 @@ class hauts_de_france_aide_garde_enfant(Variable):
         nombre_parents = famille.nb_persons(role=Famille.PARENT)
         eligibilite_statut = famille.sum(
             actifs, role=Famille.PARENT) == nombre_parents
-
         plafond_param = modalites.plafond_multiple_smic
         plafond_ressources_smic = (plafond_param.famille_monoparentale * (
             1 - couple)) + (plafond_param.famille_biparentale * couple)
