9a67a826d9a31a21a0a6deb7766e8c0b5a54c0fb
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-07-11 15:21:41 +0200

    Incrémente le numéro de version vers 6.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='OpenFisca-France-Local',
-    version='6.0.1',
+    version='6.0.2',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
