7d30b2d35016770396d4991fa642816f34e00e30
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-09-13 11:00:58 +0200

    Refactore en séquençant le code

--- openfisca_france_local/regions/nouvelle-aquitaine/aide_permis.py
+++ openfisca_france_local/regions/nouvelle-aquitaine/aide_permis.py
@@ -5,25 +5,26 @@ from openfisca_france.model.caracteristiques_socio_demographiques.logement impor
 from openfisca_france.model.prestations.education import TypesScolarite, TypesClasse
 
 
-class nouvelle_aquitaine_aide_permis(Variable):
+class eligibilite_age_nouvelle_aquitaine_aide_permis(Variable):
     value_type = float
     entity = Individu
     definition_period = MONTH
-    reference = 'https://les-aides.nouvelle-aquitaine.fr/amenagement-du-territoire/aide-au-passage-du-permis-de-conduire-b'
 
     def formula(individu, period, parameters):
-        params = parameters(period).regions.nouvelle_aquitaine.aide_permis
-
-        region_residence = individu.menage('region', period)
-        eligibilite_geographique = sum([region_residence == TypesCodeInseeRegion.nouvelle_aquitaine])
-
+        modalites_age = parameters(period).regions.nouvelle_aquitaine.aide_permis.age
         age = individu('age', period)
-        age_minimum = params.age.minimum_inclusif
-        age_maximum = params.age.maximum_inclusif
-        eligibilite_age = (age >= age_minimum) * (age <= age_maximum)
+        age_minimum = modalites_age.minimum_inclusif
+        age_maximum = modalites_age.maximum_inclusif
+        return (age >= age_minimum) * (age <= age_maximum)
 
-        activite = individu('activite', period)
-        est_chomeur = activite == TypesActivite.chomeur
+
+class eligibilite_profile_nouvelle_aquitaine_aide_permis(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+
+    def formula(individu, period):
+        est_chomeur = individu('activite', period) == TypesActivite.chomeur
 
         est_lyceen = individu('scolarite', period) == TypesScolarite.lycee
         annee_etude = individu('annee_etude', period)
@@ -33,15 +34,43 @@ class nouvelle_aquitaine_aide_permis(Variable):
 
         en_service_civique = individu('service_civique', period)
 
-        eligibilite_profile = est_chomeur + eligibilite_annee_etude + en_service_civique + est_alternant
+        return est_chomeur + eligibilite_annee_etude + en_service_civique + est_alternant
 
+
+class montant_nouvelle_aquitaine_aide_permis(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+
+    def formula(individu, period, parameters):
+        modalites_montants = parameters(period).regions.nouvelle_aquitaine.aide_permis
         rfr = individu.foyer_fiscal('rfr', period.n_2)
         nbptr = individu.foyer_fiscal('nbptr', period.n_2)
         quotient_familial = rfr / nbptr
+        
+        est_alternant = individu('alternant', period)
 
-        calcul_montant_alternants = params.montant_en_fonction_du_quotient_familial_pour_alternants.calc
-        calcul_montant = params.montant_en_fonction_du_quotient_familial.calc
-        montant = select([est_alternant, not_(est_alternant)],
-                        [calcul_montant_alternants(quotient_familial), calcul_montant(quotient_familial)])
+        calcul_montant_alternants = modalites_montants.montant_en_fonction_du_quotient_familial_pour_alternants.calc
+        calcul_montant = modalites_montants.montant_en_fonction_du_quotient_familial.calc
+        return select([est_alternant, not_(est_alternant)],
+                    [calcul_montant_alternants(quotient_familial), calcul_montant(quotient_familial)])
+
+
+class nouvelle_aquitaine_aide_permis(Variable):
+    value_type = float
+    entity = Individu
+    definition_period = MONTH
+    reference = 'https://les-aides.nouvelle-aquitaine.fr/amenagement-du-territoire/aide-au-passage-du-permis-de-conduire-b'
+
+    def formula(individu, period):
+
+        region_residence = individu.menage('region', period)
+        eligibilite_geographique = sum([region_residence == TypesCodeInseeRegion.nouvelle_aquitaine])
+
+        eligibilite_age = individu('eligibilite_age_nouvelle_aquitaine_aide_permis', period)
+
+        eligibilite_profile = individu('eligibilite_profile_nouvelle_aquitaine_aide_permis', period)
+
+        montant = individu('montant_nouvelle_aquitaine_aide_permis', period)
 
         return montant * eligibilite_geographique * eligibilite_age * eligibilite_profile
