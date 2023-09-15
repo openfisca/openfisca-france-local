b70d2c9eea40c7c45d18819a0ae36cd8ab906b1e
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-09-15 15:20:11 +0200

    Ajoute les labels et doncumente la modélisation

--- openfisca_france_local/regions/nouvelle-aquitaine/aide_permis.py
+++ openfisca_france_local/regions/nouvelle-aquitaine/aide_permis.py
@@ -9,6 +9,7 @@ class eligibilite_age_nouvelle_aquitaine_aide_permis(Variable):
     value_type = float
     entity = Individu
     definition_period = MONTH
+    label = "Éligibilité d'âge de l'aide au permis de la région Nouvelle-Aquitaine"
 
     def formula(individu, period, parameters):
         modalites_age = parameters(period).regions.nouvelle_aquitaine.aide_permis.age
@@ -22,6 +23,7 @@ class eligibilite_profile_nouvelle_aquitaine_aide_permis(Variable):
     value_type = float
     entity = Individu
     definition_period = MONTH
+    label = "Éligibilité de situation de l'aide au permis de la région Nouvelle-Aquitaine"
 
     def formula(individu, period):
         est_chomeur = individu('activite', period) == TypesActivite.chomeur
@@ -41,13 +43,14 @@ class montant_nouvelle_aquitaine_aide_permis(Variable):
     value_type = float
     entity = Individu
     definition_period = MONTH
+    label = "Montant de l'aide au permis de la région Nouvelle-Aquitaine"
 
     def formula(individu, period, parameters):
         modalites_montants = parameters(period).regions.nouvelle_aquitaine.aide_permis
         rfr = individu.foyer_fiscal('rfr', period.n_2)
         nbptr = individu.foyer_fiscal('nbptr', period.n_2)
         quotient_familial = rfr / nbptr
-        
+
         est_alternant = individu('alternant', period)
 
         calcul_montant_alternants = modalites_montants.montant_en_fonction_du_quotient_familial_pour_alternants.calc
@@ -61,9 +64,17 @@ class nouvelle_aquitaine_aide_permis(Variable):
     entity = Individu
     definition_period = MONTH
     reference = 'https://les-aides.nouvelle-aquitaine.fr/amenagement-du-territoire/aide-au-passage-du-permis-de-conduire-b'
+    label = "Éligibilité et montant associé de l'aide au permis de la région Nouvelle-Aquitaine"
+    documentation = '''
+    La modélisation de cette aide ne correspond pas à la réalité.
+    Les conditions décrites dans le paragraphe `Diplômé de la filière professionnelle ou agricole en insertion professionnelle` sont modélisées en fonction des profils susceptibles d'être intéressés lors d'une simulation avec Aides-Jeunes.
+
+    De plus, les profils décrits dans les paragraphes suivants ne sont pas modélisés :
+    - En insertion professionnelle suivi par une mission locale
+    - Scolarisé en établissement régional d’enseignement adapté (EREA)
+    '''
 
     def formula(individu, period):
-
         region_residence = individu.menage('region', period)
         eligibilite_geographique = sum([region_residence == TypesCodeInseeRegion.nouvelle_aquitaine])
 
