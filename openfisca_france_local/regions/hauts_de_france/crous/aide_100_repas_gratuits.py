2accecd01d5b23e2d9d943b401026f87e4fd0a23
Shamzic <shamery@free.fr>
2023-09-25 12:19:13 +0200

    Condition sur la région au lieu du département - aide 100 repas gratuits crous hdf & suppr. comments superflus

--- openfisca_france_local/regions/hauts_de_france/crous/aide_100_repas_gratuits.py
+++ openfisca_france_local/regions/hauts_de_france/crous/aide_100_repas_gratuits.py
@@ -1,8 +1,7 @@
 from numpy.core.defchararray import startswith
 from openfisca_france.model.prestations.education import TypesScolarite, TypesClasse
+from openfisca_france.model.caracteristiques_socio_demographiques.logement import TypesCodeInseeRegion
 from openfisca_france.model.base import Individu, MONTH, not_, Variable
-
-code_departements = [b'59', b'62']
 
 class crous_aide_100_repas_gratuits(Variable):
     value_type = float
@@ -16,24 +15,18 @@ class crous_aide_100_repas_gratuits(Variable):
 
         age = individu('age', period)
         handicap = individu('handicap', period)
-        depcom = individu.menage('depcom', period)
-        anne_etude = individu('annee_etude', period)
+        region = individu.menage('region', period)
+        annee_etude = individu('annee_etude', period)
         echelon_boursier = individu('bourse_criteres_sociaux_echelon', period)
 
-        
-        eligibilite_geographique = sum([startswith(depcom, code_departement) for code_departement in code_departements])
+        eligibilite_geographique = (region == TypesCodeInseeRegion.hauts_de_france)
 
-        # Sont éligibles les étudiants ayant moins de 35 ans (limite d’âge non applicable aux étudiants en situation de handicap)
         eligibilite_age = not_(handicap) * (age <= params.age.maximum) + handicap
 
-        # Sont éligibles les personnes inscrites dans un établissement d’enseignement supérieur, public ou privé, partenaire de la Région, reconnu par le ministère de l’Enseignement supérieur, 
-        # de la recherche et de l’innovation sur le territoire des Hauts-de-France, ou inscrites dans un établissement dispensant une formation sanitaire ou sociale gérée par la Région.
         eligibilite_scolarite = (individu('scolarite', period) == TypesScolarite.enseignement_superieur) # + (individu('scolarite', period) == TypesScolarite.formation_sanitaire_ou_sociale) @Todo : Ajouter la variable 'formation_sanitaire_ou_sociale' sur sur openfisca-france ? 
 
-        # Sont éligibles les étudiants inscrits en BTS sont exclus de ce dispositif ainsi que tous les étudiants dont l’établissement d’inscription est un lycée, tels que les étudiants en CPGE. 
-        eligibilite_annee_etude = (anne_etude != TypesClasse.cpge_1) * (anne_etude != TypesClasse.bts_1) * (anne_etude != TypesClasse.bts_2) * (anne_etude != TypesClasse.cpge_2)
+        eligibilite_annee_etude = (annee_etude != TypesClasse.cpge_1) * (annee_etude != TypesClasse.bts_1) * (annee_etude != TypesClasse.bts_2) * (annee_etude != TypesClasse.cpge_2)
 
-        # Sont éligibles les étudiants boursiers échelon 3 à 7
         eligiblite_echelon_boursier = (echelon_boursier >= params.echelon_boursier.minimum ) * (echelon_boursier <= params.echelon_boursier.maximum)
 
         return eligibilite_geographique * eligibilite_age * eligibilite_scolarite * eligibilite_annee_etude * eligiblite_echelon_boursier
