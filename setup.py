531c8c6be3834a7c704e4f38805cfb05ad486b53
baptou12 <baptou12@gmail.com>
2023-10-24 10:24:15 +0200

    met à jour Openfisca-France en version 154.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='OpenFisca-France-Local',
-    version='6.5.0',
+    version='6.6.0',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
@@ -25,7 +25,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'OpenFisca-Core >= 40.0.1, < 42',
-        'OpenFisca-France >= 153.0.1, < 154',
+        'OpenFisca-France >= 153.0.1, < 155',
         'pandas >= 1.5.3, <2.0'
         ],
     extras_require={
