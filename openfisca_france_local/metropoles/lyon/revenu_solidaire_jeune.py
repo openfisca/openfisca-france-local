4b480888143d47c04ca5a4d162bcdbad10690f30
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-11-15 11:45:56 +0100

    Modélisation des montants

--- openfisca_france_local/metropoles/lyon/revenu_solidaire_jeune.py
+++ openfisca_france_local/metropoles/lyon/revenu_solidaire_jeune.py
@@ -11,9 +11,9 @@ class revenu_solidaire_jeune(Variable):
     reference = 'https://aides-jeunes.grandlyon.com/#c21886'
 
     def formula(individu, period, parameter):
-        age_eligibles = parameter(period).metropoles.lyon.revenu_solidaire_jeune.age
+        modalites = parameter(period).metropoles.lyon.revenu_solidaire_jeune
         age = individu('age', period)
-        eligibilite_age = (age >= age_eligibles.minimum_inclusif) * (age <= age_eligibles.maximum_inclusif)
+        eligibilite_age = (age >= modalites.age.minimum_inclusif) * (age <= modalites.age.maximum_inclusif)
 
         reside_metropole_lyon = individu.menage('lyon_metropole_eligibilite_geographique', period)
 
@@ -26,4 +26,13 @@ class revenu_solidaire_jeune(Variable):
 
         pas_en_etude = individu('activite', period) != TypesActivite.etudiant
 
-        return 420 * eligibilite_age * reside_metropole_lyon * eligibilite_prestations * pas_en_etude
+        revenus_activite = (
+            individu('salaire_net', period)
+            + individu('indemnites_stage', period)
+            + individu.famille('ppa', period)
+            + individu('revenus_stage_formation_pro', period)
+            )
+
+        montant = modalites.montant.calc(revenus_activite)
+
+        return montant * eligibilite_age * reside_metropole_lyon * eligibilite_prestations * pas_en_etude
