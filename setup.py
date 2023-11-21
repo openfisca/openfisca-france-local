170d33bb846e4d946df0e49d388841bc06b1d01f
Allan - CodeWorks <allan.mamy@codeworks.fr>
2023-11-21 14:59:53 +0100

    Met à jour le setup et changelog vers la version 6.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='OpenFisca-France-Local',
-    version='6.7.2',
+    version='6.8.0',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
