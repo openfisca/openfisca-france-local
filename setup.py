7693ac93dde08d889b637f1cce61108f0c6c3318
Thomas Guillet <thomas@palya.fr>
2024-03-12 16:44:35 +0100

    Met à jour la contrainte max de version OpenFisca France (#207)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='OpenFisca-France-Local',
-    version='6.11.7',
+    version='6.11.8',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
