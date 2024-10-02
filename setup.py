86670aa46daac315334565c9f74eeac1d52de3ce
Shamzic <shamery@free.fr>
2024-10-02 17:22:53 +0200

    chore: màj version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='OpenFisca-France-Local',
-    version='6.13.0',
+    version='6.14.0',
     author='OpenFisca Team',
     author_email='contact@openfisca.fr',
     classifiers=[
