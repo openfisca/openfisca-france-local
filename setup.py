d0fe42518dd1a00e767eae18c29cce7e4bdb48cc
Shamzic <shamery@free.fr>
2024-10-21 17:49:30 +0200

    chore: màj Openfisca-Core & France

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
-        'OpenFisca-Core >= 40.0.1, < 42',
-        'OpenFisca-France >= 153.0.1, < 169',
+        'OpenFisca-Core >= 43, < 44',
+        'OpenFisca-France >= 169.0.0, < 170',
         'pandas >= 1.5.3, <2.0'
         ],
     extras_require={
