033c60e5a3e2c688c5c50b6aed3535ade01c48c3
Shamzic <shamery@free.fr>
2024-10-24 19:35:47 +0200

    chore: complète la couverture pour les tests de la tarification solidaire du périscolaire pour ll'accueil du soir en maternelle sur l'année 2024

--- openfisca_france_local/metropoles/strasbourg/tarification_solidaire_periscolaire.py
+++ openfisca_france_local/metropoles/strasbourg/tarification_solidaire_periscolaire.py
@@ -7,13 +7,10 @@ class strasbourg_metropole_quotient_familial(Variable):
     definition_period = MONTH
     label = "Quotient familial de l'eurometropole de strasbourg"
 
-    # Note: pour le moment c'est le QF de la caf, mais à terme il poura evoluer pour se baser sur les revenus imposables
     def formula(famille, period):
-        return (
-            famille.demandeur.foyer_fiscal("rfr", period.n_2)
-            / 12
-            / famille.demandeur.foyer_fiscal("nbptr", period.n_2)
-        )
+        rfr = famille.demandeur.foyer_fiscal("rfr", period.n_2)
+        nbptr = famille.demandeur.foyer_fiscal("nbptr", period.n_2)
+        return rfr / 12 / nbptr
 
 
 class strasbourg_metropole_tarification_solidaire_periscolaire(
@@ -22,18 +19,19 @@ class strasbourg_metropole_tarification_solidaire_periscolaire(
     value_type = float
     entity = Famille
     definition_period = MONTH
-    definition_period = MONTH
     label = "Tarification de l'acceuil periscolaire de l'Eurométropole de Strasbourg"
     reference = [
-        # tarifs pour 2021-2022
-        "https://www.strasbourg.eu/documents/976405/1280877/0/0cc414e4-c6b7-1709-7e1e-46b088d28805"
-        # les tarifs 2202-2023 sont
-        # https://www.strasbourg.eu/documents/976405/1280877/0/c1917f09-97b2-2604-7d58-8b8cb5c0a849
+        # tarifs pour 2021-2022 :
+        "https://www.strasbourg.eu/documents/976405/1280877/0/0cc414e4-c6b7-1709-7e1e-46b088d28805",
+        # tarifs 2202-2023
+        "https://www.strasbourg.eu/documents/976405/1280877/0/c1917f09-97b2-2604-7d58-8b8cb5c0a849",
+        # tarifs 2024-2025 :
+        "https://www.strasbourg.eu/documents/976405/1280877/Tarifs-services-periscolaires-2024-2025.pdf/62fa6543-19ae-b69d-3313-13e881a6b866?version=1.0&t=1723127337051"
     ]
 
     def formula(famille, period, parameters):
-        qf = famille("strasbourg_metropole_quotient_familial", period)
+        quotient_familial = famille("strasbourg_metropole_quotient_familial", period)
         tarif = parameters(
             period
-        ).metropoles.strasbourg.periscolaire.acceuil_matin_soir_maternelle
-        return tarif.calc(max_(0, qf), right=True)
+        ).metropoles.strasbourg.periscolaire.acceuil_soir_maternelle
+        return tarif.calc(quotient_familial)
