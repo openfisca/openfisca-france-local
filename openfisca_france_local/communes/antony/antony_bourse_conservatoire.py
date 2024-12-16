842979976774c5f76abfa542f57c3172a946bdab
c82ogui <ext_guill.robin@imsa.msa.fr>
2024-12-16 15:55:02 +0100

    feat(antony): ajout scolarisation

--- openfisca_france_local/communes/antony/antony_bourse_conservatoire.py
+++ openfisca_france_local/communes/antony/antony_bourse_conservatoire.py
@@ -1,5 +1,6 @@
  # -*- coding: utf-8 -*-
-from openfisca_france.model.base import Famille, MONTH, Variable
+from openfisca_france.model.base import *  # noqa analysis:ignore
+from openfisca_france.model.prestations.education import TypesScolarite
 
 
 class antony_bourse_conservatoire(Variable):
@@ -14,8 +15,13 @@ class antony_bourse_conservatoire(Variable):
 
         residence_antony = famille.demandeur.menage('antony_eligibilite_residence', period)
         condition_ressources_remplies = famille('antony_eligibilite_ressources', period)
+
+        scolarite = famille.members('scolarite', period)
+        scolarise_i = ((scolarite == TypesScolarite.college) + (scolarite == TypesScolarite.lycee))
+
         age_i = famille.members('age', period)
+        moins_de_18_ans_scolarise = scolarise_i * (age_i < 18)
 
-        au_moins_un_enfant_moins_de_18_ans = famille.any(age_i < age_maximum, role=Famille.ENFANT)
+        au_moins_un_scolarise_18_ans = famille.any(moins_de_18_ans_scolarise, role=Famille.ENFANT)
 
-        return residence_antony * condition_ressources_remplies * au_moins_un_enfant_moins_de_18_ans
+        return residence_antony * condition_ressources_remplies * au_moins_un_scolarise_18_ans
