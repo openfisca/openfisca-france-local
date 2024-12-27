599069c623b7b39998dd63ced96be9ad143ec9d3
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2024-12-27 15:53:23 +0100

    fix: verificaiton de plusieurs code postal

--- openfisca_france_local/communes/montpellier/eligibilite_metropole_mutuelle.py
+++ openfisca_france_local/communes/montpellier/eligibilite_metropole_mutuelle.py
@@ -8,10 +8,11 @@ class montpellier_eligibilite_residence(Variable):
     reference = "https://encommun.montpellier.fr/articles/2024-12-19-lagence-de-la-mutuelle-communale-de-montpellier-est-ouverte"
 
     def formula(menage, period):
-        return menage('depcom', period) in [b'34172', #Montpellier
-                                            b'34077', #Clapier
-                                            b'34090', #Le Crès
-                                            b'34179', #Murviel-lès-Montpellier
-                                            b'34256', #Saint-Geniès-des-Mourgues
-                                            b'34307' #Sussargues
-                                            ]
+        return any(menage('depcom', period) == code for code in [
+            b'34172',  # Montpellier
+            b'34077',  # Clapier
+            b'34090',  # Le Crès
+            b'34179',  # Murviel-lès-Montpellier
+            b'34256',  # Saint-Geniès-des-Mourgues
+            b'34307'   # Sussargues
+        ])
