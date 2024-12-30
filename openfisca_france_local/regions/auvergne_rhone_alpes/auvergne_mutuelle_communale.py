2de7d027c880da777c1445f5ddb1c18000cdf60a
Jeremy PASTOURET <pastouret.jeremy@gmail.com>
2024-12-30 17:45:04 +0100

    feat: ajout de la mutuelle rhone alpes

---
+++ openfisca_france_local/regions/auvergne_rhone_alpes/auvergne_mutuelle_communale.py
@@ -0,0 +1,21 @@
+from openfisca_france.model.base import Variable, MONTH, not_, Famille
+from openfisca_france.model.caracteristiques_socio_demographiques.logement \
+    import TypesCodeInseeRegion
+
+class auvergne_mutuelle_communale(Variable):
+    value_type = bool
+    entity = Famille
+    definition_period = MONTH
+    label = "Mutuelle communale de la région Auvergne Rhône Alpes"
+    reference = [
+        "https://www.auvergnerhonealpes.fr/particuliers/mutuelleetudiants"
+        ]
+
+    def formula(famille, period):
+        region = famille.demandeur.menage('region', period)
+        eligibilite_residentielle = (
+                region == TypesCodeInseeRegion.auvergne_rhone_alpes)
+        css_participation_forfaitaire = famille('css_participation_forfaitaire', period)
+        cmu_c = famille('cmu_c', period)
+        eligibilite_css_cmu_c = (css_participation_forfaitaire > 0) | cmu_c
+        return eligibilite_residentielle * not_(eligibilite_css_cmu_c)
